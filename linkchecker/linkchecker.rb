1676a27c68ca106f64bfacbd7016b6f5cf8a47c8
Matt House <matt@theshadowaspect.com>
2009-04-24 12:28:15 +0100

    added linkchecker script to parse the downloaded csv's from google webmaster tools and check whether the response codes have changed.

---
+++ linkchecker/linkchecker.rb
@@ -0,0 +1,17 @@
+require 'net/http'
+require 'csv'
+
+# Parse the output of Google webmasters tools csv data to see if the response codes have changed
+
+domain="www.example.com"
+
+parsed_file=CSV::Reader.parse(File.open('csv.csv', 'r'))
+
+parsed_file.each do |row|
+  url = row[0].gsub('http://#{domain}',"")
+  resp=""
+  Net::HTTP.start(domain) { |http|
+    resp=http.get(url)
+  }
+  p resp.code << " : " << url 
+end
