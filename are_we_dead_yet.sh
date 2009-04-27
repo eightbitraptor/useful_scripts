74ab13de0479817314fc77339103c660ff254358
Matt House <matt@theshadowaspect.com>
2009-04-27 16:50:43 +0100

    math fail: don't need to use bc, just multiply before the division, also neatening up and making more efficient. It's a really bad idea to awk the load average from uptime as the number of columns can change

--- are_we_dead_yet.sh
+++ are_we_dead_yet.sh
@@ -1,25 +1,19 @@
 #! /bin/bash
-
+PATH=/bin:/usr/bin
 # grab the load average for the last minute and convert it to an integer
-loadavg=`uptime | awk '{print $10}'`
-thisloadavg=`echo $loadavg|awk -F \. '{print $1}'`
-declare -i thisloadavg
-echo $thisloadavg
-
+thisloadavg=`cat /proc/loadavg | awk -F \. '{print $1}'`
 psfile=ps-`date +%y%m%d-%H%M`.txt
-
-if [ -d ~/pstmp ]; then
-	echo "nothing to do"
-else
-	mkdir ~/pstmp
-fi
 
 usedmem=`free -m | grep "buffers/cache" | awk '{print $4}'`
 totmem=`free -m | grep Mem | awk '{print $2}'`
 
-memperc=`echo "scale=3;$usedmem/$totmem * 100" | bc | awk -F \. '{print $1}'`
-echo $memperc
+let memperc=(usedmem*100)/totmem
 
 if [ $memperc -lt 10 ] || [ $thisloadavg -gt 10 ]; then
-	ps -ef > ~/pstmp/$psfile
+	mkdir -p ~/pstmp
+	uptime > ~/pstmp/$psfile
+	ps -ef >> ~/pstmp/$psfile
+	free -m >> ~/pstmp/$psfile
+else
+	echo "load: $thisloadavg, memperc: $memperc"
 fi
