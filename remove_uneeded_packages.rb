a0627d7f4c0c9f671b07adbbc7a4dfd19baaf596
Matt House <matt@theshadowaspect.com>
2009-07-23 08:50:33 +0100

    Completely unnecessary and potentially dangerous bit of Arch Linux packaging foo. Stop being lazy and manage your packages properly.

--- remove_uneeded_packages.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# find all packages installed as deps that are orphaned
-`pacman -Qdt`.each do |package|
-  # and remove them (but not their deps)
-  `sudo pacman -R #{package.gsub(/ .*/, "")}`
-end
